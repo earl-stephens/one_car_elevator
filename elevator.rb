bf4ac4376a467f38f667d46391317ed77bc9da04
earl-stephens <stephensearl@hotmail.com>
2020-02-13 22:50:16 -0500

    Adds test for elevator moving to called floor
    Added a test for the elevator to move to a floor that it is called from. 
     Wrote the test only - code is not yet written.
    
    Add a couple more instance variables to the initialization method.

--- elevator.rb
+++ elevator.rb
@@ -1,9 +1,13 @@
 class Elevator
   attr_reader :max_floors,
-              :parked_floor
+              :parked_floor,
+              :queued_floors,
+              :current_floor
 
   def initialize(max_floor_number)
     @max_floors = max_floor_number
     @parked_floor = 1
+    @queued_floors = []
+    @current_floor = 1
   end
 end
